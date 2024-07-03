41db73c7046a42612e0349b2cdd7757d1df2f90f
Phemtech <43777555+ProgHubng@users.noreply.github.com>
2024-07-04 00:12:25 +0100

    add
    Get requester Name, Location and temperature

---
+++ api/hello.php
@@ -0,0 +1,21 @@
+<?php
+$visitor_name = isset($_GET['visitor_name']) ? htmlspecialchars($_GET['visitor_name']) : 'Mark';
+$client_ip = $_SERVER['REMOTE_ADDR'];
+// $location = "New York";
+$temperature = 11;
+
+$query = @unserialize(file_get_contents('http://ip-api.com/php/' . $client_ip));
+if ($query && $query['status'] == 'success') {
+    $location =  $query['city'];
+}
+
+
+$response = [
+    'client_ip' => $client_ip,
+    'location' => $location,
+    'greeting' => "Hello, $visitor_name!, the temperature is $temperature degrees Celsius in $location"
+];
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+echo json_encode($response);
